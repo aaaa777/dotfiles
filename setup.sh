d8aeebe88391f05c20ab5f0f9c6e779d1da068f9
aaaa777 <aaaa777@users.noreply.github.com>
2023-03-14 22:58:34 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+# check root
+if [ "root" = "$(whoami)" ]; then
+  SUDO="sudo"
+fi
+
 # copy dotfiles
-echo "setting .bash_aliases"
+echo "setting .bash_*"
 if [ -e "~/.bash_aliases" ]; then
   cat .bash_aliases >> ~/.bash_aliases
 else
   cp -p .bash_aliases ~/
+fi
+
+if [ -e "~/.bash_profile" ]; then
+  cat .bash_profile >> ~/.bash_profile
+else
+  if [ -e "~/.profile" ]; then
+    cp -p ~/.profile ~/.bash_profile
+    cat .bash_profile >> ~/.bash_profile
+  else
+    cp -p .bash_profile ~/
+  fi
+fi
+
+if [ -e "~/.bashrc" ]; then
+  cat .bashrc >> ~/.bashrc
+else
+  cp -p .bashrc ~/
 fi
 
 # configure git
@@ -16,7 +38,7 @@ git config --global user.email aaaa777@users.noreply.github.com
 # install github-cli
 echo "checking github-cli"
 if [ -z "$(which gh)" ]; then
-  sudo apt update && sudo apt install gh && gh auth login
+  $SUDO apt update && $SUDO apt install gh && gh auth login
 fi
 
 # apply sources
